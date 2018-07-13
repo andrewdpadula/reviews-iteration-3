bdda44c60575e6039c5763377c8ba5ec371fc3d7
Andrew Padula <andrewdpadula@gmail.com>
2018-07-13 10:11:55 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/reviewsiteration3/ReviewsIteration3Application.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.reviewsiteration3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsIteration3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsIteration3Application.class, args);
+	}
+}
